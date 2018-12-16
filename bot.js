7a2517f7323fa212cea942a20d55f67a6130cd2a
adasfkdghkjhdkjghdskgk <44849612+adasfkdghkjhdkjghdskgk@users.noreply.github.com>
2018-12-16 04:11:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-client.user.setGame(`Arab Community SERVER`,"https://www.twitch.tv/idk")
+client.user.setGame(`LaST.Community©  BOT BY:|ADAM|أدم|`,"https://www.twitch.tv/idk")
   console.log('---------------');
   console.log(' Bot Is Online')
   console.log('---------------')
